e983f26e6c66bb6eb9acba42020f39c4329e1606
pietro <pietro.lima.queiroz@gmail.com>
2024-02-05 15:57:42 -0400

    step 1 website

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,13 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+//aqui
+import { Inter, Poppins, Prosto_One } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
-const inter = Inter({ subsets: ["latin"] });
+import localFont from "@next/font/local";
+
+const inter = Poppins({ weight: "400", subsets: ["latin"] });
+
 
 export const metadata: Metadata = {
   title: "Create Next App",
