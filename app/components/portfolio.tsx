a87e052896973e52566cf71e6f037d7ea0c2273d
pietro <pietro.lima.queiroz@gmail.com>
2024-02-06 20:54:23 -0400

    optimized

--- app/components/portfolio.tsx
+++ app/components/portfolio.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 
 function Project({
@@ -14,7 +15,11 @@ function Project({
       <Link href={url}>
         <div className="flex flex-col gap-4 items-center rounded-xl">
           <div className="rounded-full p-4">
-            <img src={image} alt={description} className="h-36 rounded-full" />
+            <Image
+              src={image}
+              alt={description}
+              className="h-36 rounded-full"
+            />
           </div>
           <p className="text-black px-4">{description}</p>
         </div>
